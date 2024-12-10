8bd8932193205e6dfcead185056832209339f833
sathvikchandanala <sathvikchandanala@gmail.com>
2024-12-10 17:24:30 +0530

    first commit

---
+++ backend/database/connection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const connection = ()=>{
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "JOB_PORTAL_WITH_AUTOMATION"
+    }).then(()=>{
+        console.log("Connected to database.")
+    }).catch(err=>{
+        console.log(`Some error occured while connecting to database: ${err}`)
+    })
+}
---
+++ backend/utils/jwtToken.js
@@ -0,0 +1,18 @@
+export const sendToken = (user, statusCode, res, message) => {
+  const token = user.getJWTToken();
+  const options = {
+    expires: new Date(
+      Date.now() + (process.env.COOKIE_EXPIRE || 7) * 24 * 60 * 60 * 1000 // Default to 7 days if COOKIE_EXPIRE is undefined
+    ),
+    httpOnly: true, // Prevent client-side access
+    secure: "development", // Secure in production
+    sameSite: "Strict", // Adjust to your use case, e.g., 'Lax' or 'None'
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    message,
+    token,
+  });
+};
